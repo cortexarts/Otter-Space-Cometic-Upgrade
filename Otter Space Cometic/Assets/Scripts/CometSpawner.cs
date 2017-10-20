febbc87563deb60058877251fef5c0d986a86917
JorisRoelofs <joris.roelofs@hccnet.nl>
2017-10-20 14:07:46 +0200

    Comet formula
    Comets now display randomly generated formulas, whose values are randomly generated in CometSpawner and accesible.

--- Otter Space Cometic/Assets/Scripts/CometSpawner.cs
+++ Otter Space Cometic/Assets/Scripts/CometSpawner.cs
@@ -10,6 +10,12 @@ public class CometSpawner : MonoBehaviour
     public GameObject cometPrefab;
     public List<GameObject> comets;
 
+    public int valueAnswerMin = 1;
+    public int valueAnswerMax = 10;
+    public int valueMultiplicationMin = 1;
+    public int valueMultiplicationMax = 10;
+
+
     // Use this for initialization
     void Start ()
     {
@@ -22,7 +28,9 @@ public class CometSpawner : MonoBehaviour
             GameObject newComet = Instantiate(cometPrefab, this.transform.position + new Vector3(Random.Range(offsetExtrema.x, offsetExtrema.y), Random.Range(offsetExtrema.x, offsetExtrema.y), 0), Quaternion.identity) as GameObject;
             newComet.transform.localScale = new Vector3(Random.Range(scaleExtrema.x, scaleExtrema.y), Random.Range(scaleExtrema.x, scaleExtrema.y), 1);
             comets.Add(newComet);
-            newComet.SendMessage("SetAnswer", 3);
+
+            List<int> value = new List<int> { Random.Range(valueAnswerMin, valueAnswerMax), Random.Range(valueMultiplicationMin, valueMultiplicationMax)};
+            newComet.SendMessage("SetAnswer", value);
         }
 	}
 	
--- Otter Space Cometic/Assets/Scripts/CometValues.cs
+++ Otter Space Cometic/Assets/Scripts/CometValues.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CometValues : MonoBehaviour
 {
-    public float answer;
+    public List<int> value = new List<int>();
+    public int answer;
+    public int multiplication;
+    public int offset;
+    public Text formula;
+
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-    void SetAnswer(float a_Answer)
+    void SetAnswer(List<int> value)
     {
-        answer = a_Answer;
+        answer = value[0];
+        multiplication = value[1];
+        offset = answer * multiplication;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        formula.text = multiplication.ToString() + "x = " + offset.ToString();
+
+    }
 }
