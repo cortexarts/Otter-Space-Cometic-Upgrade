983a572b37c736adbda161548212a366e4903106
JorisRoelofs <joris.roelofs@hccnet.nl>
2017-10-20 15:24:38 +0200

    More advanced formula
    Formula's can now be 2nd power, a difficulty public int is added and formula's now look cleaner (e.g.g 1x is replaced by x)

--- Otter Space Cometic/Assets/Scripts/CometSpawner.cs
+++ Otter Space Cometic/Assets/Scripts/CometSpawner.cs
@@ -10,11 +10,28 @@ public class CometSpawner : MonoBehaviour
     public GameObject cometPrefab;
     public List<GameObject> comets;
 
+    public int difficulty;
     public int valueAnswerMin = 1;
     public int valueAnswerMax = 10;
-    public int valueMultiplicationMin = 1;
-    public int valueMultiplicationMax = 10;
+    public int valueLinearMultiplicationMin = 1;
+    public int valueLinearMultiplicationMax = 10;
+    public int valueSquareMultiplicationMin = 1;
+    public int valueSquareMultiplicationMax = 10;
+    public int valueOffsetVariationMin = 1;
+    public int valueOffsetVariationMax = 10;
 
+
+    void setRanges(int value1, int value2, int value3, int value4, int value5, int value6, int value7, int value8)
+    {
+        valueAnswerMin = value1;
+        valueAnswerMax = value2;
+        valueSquareMultiplicationMin = value3;
+        valueSquareMultiplicationMax = value4;
+        valueLinearMultiplicationMin = value5;
+        valueLinearMultiplicationMax = value6;
+        valueOffsetVariationMin = value7;
+        valueOffsetVariationMax = value8;
+}
 
     // Use this for initialization
     void Start ()
@@ -29,7 +46,21 @@ public class CometSpawner : MonoBehaviour
             newComet.transform.localScale = new Vector3(Random.Range(scaleExtrema.x, scaleExtrema.y), Random.Range(scaleExtrema.x, scaleExtrema.y), 1);
             comets.Add(newComet);
 
-            List<int> value = new List<int> { Random.Range(valueAnswerMin, valueAnswerMax), Random.Range(valueMultiplicationMin, valueMultiplicationMax)};
+
+
+            switch (difficulty)
+            {
+                case 0: setRanges(0, 10, 0, 0, 1, 1, 0, 0); break;
+                case 1: setRanges(0, 10, 0, 0, 1, 5, 0, 0); break;
+                case 2: setRanges(0, 10, 0, 0, 1, 5, 0, 10); break;
+                case 3: setRanges(0, 10, 0, 0, 1, 10, 0, 30); break;
+                case 4: setRanges(0, 10, 1, 1, 0, 0, 0, 0); break;
+                case 5: setRanges(0, 10, 1, 1, 1, 5, 0, 0); break;
+                case 6: setRanges(0, 10, 0, 1, 1, 10, 0, 30); break;
+                case 7: setRanges(0, 10, 0, 2, 1, 10, 0, 30); break;
+            }
+
+            List<int> value = new List<int> { Random.Range(valueAnswerMin, valueAnswerMax), Random.Range(valueLinearMultiplicationMin, valueLinearMultiplicationMax), Random.Range(valueSquareMultiplicationMin, valueSquareMultiplicationMax), Random.Range(valueOffsetVariationMin, valueOffsetVariationMax) };
             newComet.SendMessage("SetAnswer", value);
         }
 	}
--- Otter Space Cometic/Assets/Scripts/CometValues.cs
+++ Otter Space Cometic/Assets/Scripts/CometValues.cs
@@ -3,30 +3,105 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
 public class CometValues : MonoBehaviour
 {
-    public List<int> value = new List<int>();
+    private List<int> value = new List<int>();
+    private int offset;
+    private string stringToReturn;
+    private string stringToAdd;
+
     public int answer;
-    public int multiplication;
-    public int offset;
+    public int linearMultiplication;
+    public int squareMultiplication;
+    public int offsetLeft;
+    public int offsetRight;
     public Text formula;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
 
     void SetAnswer(List<int> value)
     {
         answer = value[0];
-        multiplication = value[1];
-        offset = answer * multiplication;
+        linearMultiplication = value[1];
+        squareMultiplication = value[2];
+        offset = -((answer * answer * squareMultiplication) + (answer * linearMultiplication));
+        offsetLeft = offset - value[3];
+        offsetRight = offset - offsetLeft;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        formula.text = multiplication.ToString() + "x = " + offset.ToString();
 
+    string cleanupText(int number, string stringToAdd)
+    {
+        stringToReturn = "";
+        if (number != 0)
+        {
+            if (number != 1) stringToReturn += number.ToString();
+            if (stringToAdd != "")
+            {
+                stringToReturn += stringToAdd;
+                if (number > 0)
+                {
+                    stringToReturn += "+ ";
+                }
+            }
+        }
+        return stringToReturn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        formula.text = "";
+        formula.text += cleanupText(squareMultiplication, "x^2 ");
+        formula.text += cleanupText(linearMultiplication, " x ");
+        formula.text += cleanupText(offsetLeft, "");
+        formula.text += " = ";
+
+        if (offsetRight != 0) formula.text += cleanupText(offsetRight, "");
+        else formula.text += "0";
     }
 }
+
+//public class CometValues : MonoBehaviour
+//{
+//    private List<int> value = new List<int>();
+//    private int offset;
+
+//    public int answer;
+//    public int linearMultiplication;
+//    public int squareMultiplication;
+//    public int offsetLeft;
+//    public int offsetRight;
+//    public Text formula;
+
+
+//	// Use this for initialization
+//	void Start () {
+
+//	}
+
+//    void SetAnswer(List<int> value)
+//    {
+//        answer = value[0];
+//        linearMultiplication = value[1];
+//        squareMultiplication = value[2];
+//        offset = (answer * answer * squareMultiplication) + (answer * linearMultiplication);
+//        offsetLeft = offset + value[3];
+//        offsetRight = offset - offsetLeft;
+//    }
+
+//	// Update is called once per frame
+//	void Update () {
+//        formula.text = squareMultiplication.ToString() + "x^2 + " + linearMultiplication.ToString() + " x + " + offsetLeft.ToString() + " = " + offsetRight.ToString();
+
+//    }
+//}
