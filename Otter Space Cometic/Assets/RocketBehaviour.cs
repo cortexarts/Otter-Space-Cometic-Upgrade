6fcd703a94169833dac99f5dad66c3b76f3f5911
JasperDre <jasperdre@gmail.com>
2017-10-20 14:33:45 +0200

    Added Rockets

---
+++ Otter Space Cometic/Assets/RocketBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehaviour : MonoBehaviour
+{
+    public float lifeTime = 0;
+    public float maxLifeTime = 5.0f;
+    public float movementSpeed = 2.0f;
+
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lifeTime += Time.deltaTime;
+
+        if (lifeTime > maxLifeTime)
+        {
+            DestroyImmediate(this.gameObject);
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.up * movementSpeed * Time.deltaTime, ForceMode2D.Impulse);
+    }
+}
---
+++ Otter Space Cometic/Assets/RocketShooting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShooting : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+
+    // Handle camera shaking
+    public float camShakeAmt = 0.05f;
+    public float camShakeLength = 0.1f;
+   // CameraShake camShake;
+
+    // Use this for initialization
+    void Start ()
+    {
+      //  camShake = GameMaster.gm.GetComponent<CameraShake>();
+      //  if (camShake == null)
+       //     Debug.LogError("No CameraShake script found on GM object.");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject newRocket = Instantiate(rocketPrefab, this.transform.position, this.transform.rotation) as GameObject;
+
+        //if (hitNormal != new Vector3(9999, 9999, 9999))
+        //{
+        //    Transform hitParticle = Instantiate(HitPrefab, hitPos, Quaternion.FromToRotation(Vector3.right, hitNormal)) as Transform;
+        //    Destroy(hitParticle.gameObject, 1f);
+        //}
+
+        //Shake the camera
+        //camShake.Shake(camShakeAmt, camShakeLength);
+    }
+}
