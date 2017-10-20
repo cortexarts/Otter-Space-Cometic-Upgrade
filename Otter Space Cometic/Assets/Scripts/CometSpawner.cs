2fa17694cc13b27a8703e05a81fcd586bc7a81d0
JasperDre <jasperdre@gmail.com>
2017-10-20 16:02:48 +0200

    Changed method names and random positions

--- Otter Space Cometic/Assets/Scripts/CometSpawner.cs
+++ Otter Space Cometic/Assets/Scripts/CometSpawner.cs
@@ -20,8 +20,7 @@ public class CometSpawner : MonoBehaviour
     public int valueOffsetVariationMin = 1;
     public int valueOffsetVariationMax = 10;
 
-
-    void setRanges(int value1, int value2, int value3, int value4, int value5, int value6, int value7, int value8)
+    void SetRanges(int value1, int value2, int value3, int value4, int value5, int value6, int value7, int value8)
     {
         valueAnswerMin = value1;
         valueAnswerMax = value2;
@@ -31,7 +30,7 @@ public class CometSpawner : MonoBehaviour
         valueLinearMultiplicationMax = value6;
         valueOffsetVariationMin = value7;
         valueOffsetVariationMax = value8;
-}
+    }
 
     // Use this for initialization
     void Start ()
@@ -40,24 +39,40 @@ public class CometSpawner : MonoBehaviour
 
         comets = new List<GameObject>();
 
+        Vector3 newPosition;
+        Vector3 lastPosition = new Vector3(0,0,0);
+
         for (int i = 0; i < cometsCount; i++)
         {
-            GameObject newComet = Instantiate(cometPrefab, this.transform.position + new Vector3(Random.Range(offsetExtrema.x, offsetExtrema.y), Random.Range(offsetExtrema.x, offsetExtrema.y), 0), Quaternion.identity) as GameObject;
+            // Generate random position that isn't overlapping with the previous position
+            newPosition = this.transform.position + new Vector3(Random.Range(offsetExtrema.x, offsetExtrema.y), Random.Range(offsetExtrema.x, offsetExtrema.y), 0);
+            if (newPosition.x - 2.0f > lastPosition.x && newPosition.y - 2.0f > lastPosition.y)
+            {
+                // Positions should be atleast 2.0f units apart
+            }
+            else
+            {
+                // Regenerate positition
+                newPosition = this.transform.position + new Vector3(Random.Range(offsetExtrema.x, offsetExtrema.y), Random.Range(offsetExtrema.x, offsetExtrema.y), 0);
+            }
+
+            GameObject newComet = Instantiate(cometPrefab, newPosition, Quaternion.identity) as GameObject;
             newComet.transform.localScale = new Vector3(Random.Range(scaleExtrema.x, scaleExtrema.y), Random.Range(scaleExtrema.x, scaleExtrema.y), 1);
             comets.Add(newComet);
 
-
+            // Store current position
+            lastPosition = newPosition;
 
             switch (difficulty)
             {
-                case 0: setRanges(0, 10, 0, 0, 1, 1, 0, 0); break;
-                case 1: setRanges(0, 10, 0, 0, 1, 5, 0, 0); break;
-                case 2: setRanges(0, 10, 0, 0, 1, 5, 0, 10); break;
-                case 3: setRanges(0, 10, 0, 0, 1, 10, 0, 30); break;
-                case 4: setRanges(0, 10, 1, 1, 0, 0, 0, 0); break;
-                case 5: setRanges(0, 10, 1, 1, 1, 5, 0, 0); break;
-                case 6: setRanges(0, 10, 0, 1, 1, 10, 0, 30); break;
-                case 7: setRanges(0, 10, 0, 2, 1, 10, 0, 30); break;
+                case 0: SetRanges(0, 10, 0, 0, 1, 1, 0, 0); break;
+                case 1: SetRanges(0, 10, 0, 0, 1, 5, 0, 0); break;
+                case 2: SetRanges(0, 10, 0, 0, 1, 5, 0, 10); break;
+                case 3: SetRanges(0, 10, 0, 0, 1, 10, 0, 30); break;
+                case 4: SetRanges(0, 10, 1, 1, 0, 0, 0, 0); break;
+                case 5: SetRanges(0, 10, 1, 1, 1, 5, 0, 0); break;
+                case 6: SetRanges(0, 10, 0, 1, 1, 10, 0, 30); break;
+                case 7: SetRanges(0, 10, 0, 2, 1, 10, 0, 30); break;
             }
 
             List<int> value = new List<int> { Random.Range(valueAnswerMin, valueAnswerMax), Random.Range(valueLinearMultiplicationMin, valueLinearMultiplicationMax), Random.Range(valueSquareMultiplicationMin, valueSquareMultiplicationMax), Random.Range(valueOffsetVariationMin, valueOffsetVariationMax) };
--- Otter Space Cometic/Assets/Scripts/CometValues.cs
+++ Otter Space Cometic/Assets/Scripts/CometValues.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine;
-
 public class CometValues : MonoBehaviour
 {
     private List<int> value = new List<int>();
@@ -39,7 +34,7 @@ public class CometValues : MonoBehaviour
         offsetRight = offset - offsetLeft;
     }
 
-    string cleanupText(int number, string stringToAdd)
+    string CleanupText(int number, string stringToAdd)
     {
         stringToReturn = "";
         if (number != 0)
@@ -61,47 +56,12 @@ public class CometValues : MonoBehaviour
     void Update()
     {
         formula.text = "";
-        formula.text += cleanupText(squareMultiplication, "x^2 ");
-        formula.text += cleanupText(linearMultiplication, " x ");
-        formula.text += cleanupText(offsetLeft, "");
+        formula.text += CleanupText(squareMultiplication, "x^2 ");
+        formula.text += CleanupText(linearMultiplication, " x ");
+        formula.text += CleanupText(offsetLeft, "");
         formula.text += " = ";
 
-        if (offsetRight != 0) formula.text += cleanupText(offsetRight, "");
+        if (offsetRight != 0) formula.text += CleanupText(offsetRight, "");
         else formula.text += "0";
     }
 }
-
-//public class CometValues : MonoBehaviour
-//{
-//    private List<int> value = new List<int>();
-//    private int offset;
-
-//    public int answer;
-//    public int linearMultiplication;
-//    public int squareMultiplication;
-//    public int offsetLeft;
-//    public int offsetRight;
-//    public Text formula;
-
-
-//	// Use this for initialization
-//	void Start () {
-
-//	}
-
-//    void SetAnswer(List<int> value)
-//    {
-//        answer = value[0];
-//        linearMultiplication = value[1];
-//        squareMultiplication = value[2];
-//        offset = (answer * answer * squareMultiplication) + (answer * linearMultiplication);
-//        offsetLeft = offset + value[3];
-//        offsetRight = offset - offsetLeft;
-//    }
-
-//	// Update is called once per frame
-//	void Update () {
-//        formula.text = squareMultiplication.ToString() + "x^2 + " + linearMultiplication.ToString() + " x + " + offsetLeft.ToString() + " = " + offsetRight.ToString();
-
-//    }
-//}
