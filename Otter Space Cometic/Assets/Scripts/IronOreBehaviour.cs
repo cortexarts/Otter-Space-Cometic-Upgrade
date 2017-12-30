9f8efd9647180608f7e5dbda8864b6a68c8d73e4
JasperDre <jasperdre@gmail.com>
2017-12-30 12:43:51 +0100

    [CHANGED] Missile speed and iron script

--- Otter Space Cometic/Assets/Scripts/IronOreBehaviour.cs
+++ Otter Space Cometic/Assets/Scripts/IronOreBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IronOreBehavior : MonoBehaviour
+public class IronOreBehaviour : MonoBehaviour
 {
 
 	// Use this for initialization
